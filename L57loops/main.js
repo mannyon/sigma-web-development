efd4be05058fee7663ff9a9a71ff34c866d80db5
Manish <mannyinwork@gmail.com>
2024-03-30 17:56:07 +0530

    msg

---
+++ L57loops/main.js
@@ -0,0 +1,35 @@
+// for loop 
+// for (let i=1; i<11; i++){
+//     console.log(i);
+// }
+
+// for in loop 
+// let obj = {
+//     name : "Manish",
+//     role : "Programmer",
+//     "company name" : "Google"
+// }
+// for (const key in obj) {
+//     const Element = obj[key];
+//     console.log(Element);
+// }
+
+// for of loop
+// for (const c of "Manish"){
+//     console.log(c);
+// }
+
+// while loop 
+// let i = 0;
+// while (i<6){
+//     console.log(i);
+//      i++; 
+// }
+
+
+// do while loop
+let i = 0;
+do {
+    console.log(i);
+    i++;
+    }while (i<6);
